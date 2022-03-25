f0f7488c159f9888ea5e51848673ab97aa36cf9a
dManh6302 <91354019+dManh6302@users.noreply.github.com>
2022-03-25 08:24:24 +0700

    Add files via upload
    add commandClass

---
+++ CommandClass.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+#include "userClass.cpp"
+using namespace std;
+
+class Command
+{
+private:
+    vector<User> users;
+    User user;
+
+public:
+    Command()
+    {
+        user.input();
+    }
+
+};
